9d0fdcaff4e5f6e52e4e97ac69e2ca11d24ed6cb
Henrique Lorentz <lorentzhenrique1@gmail.com>
2026-06-14 15:57:09 +0100

    Edição final para o Vercel.

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://pw-trabalho-pratico.vercel.app/',
+  baseURL: 'https://pw-api-nova.vercel.app',
 });
 
 api.interceptors.request.use((config) => {
